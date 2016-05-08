08be7c6d9834a7e83a34c21c2218130fc9464927
Ubuntu <erpuser@erp6dev5.erp6dev5.i5.internal.cloudapp.net>
2016-05-08 10:26:00 +0000

    Added Validation for Mobile No

--- myfirstapp/my_first_frappe_app/doctype/first_doctype/first_doctype.js
+++ myfirstapp/my_first_frappe_app/doctype/first_doctype/first_doctype.js
@@ -6,3 +6,22 @@ frappe.ui.form.on('First DocType', {
 
 	}
 });
+
+
+frappe.ui.form.on("First DocType", "validate", function(frm){
+ if (!isMobileNoValid(frm.doc.contact_number)) {
+ flashMessage();
+ validated = false;
+ }
+});
+
+function isMobileNoValid(mobno) {
+ var rx = new RegExp("^[789]\\\\d{9}$"); 
+ var isvalid = rx.test(mobno);
+ return isvalid;
+}
+
+function flashMessage() {
+ msgprint(__("Entered Mobile No. is either incomplete or invalid. <br /> Tip: Add it without the +0 or +91 at the beginning.", "Invalid Mobile No"));
+}
+
